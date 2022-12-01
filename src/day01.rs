6c3b4dd28b6bc4b619837b558db0cd5b2a3cc3ce
Gabriel Ivașcu <ivascu.gabriel59@gmail.com>
2022-12-01 11:49:17 +0200

    Solve day 01

--- src/day01.rs
+++ src/day01.rs
@@ -1,7 +1,18 @@
+use itertools::Itertools;
+
 pub fn solve_1() -> i64 {
-    -1
+    include_str!("../input/01.txt")
+        .split("\n\n")
+        .map(|x| x.lines().map(|y| y.parse::<i64>().unwrap()).sum::<i64>())
+        .max()
+        .unwrap()
 }
 
 pub fn solve_2() -> i64 {
-    -1
+    include_str!("../input/01.txt")
+        .split("\n\n")
+        .map(|x| x.lines().map(|y| y.parse::<i64>().unwrap()).sum::<i64>())
+        .sorted_by(|a, b| Ord::cmp(b, a))
+        .take(3)
+        .sum()
 }
