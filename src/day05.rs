001804ac02240207736e41c572211e615db34d56
Gabriel Ivașcu <ivascu.gabriel59@gmail.com>
2022-12-05 12:31:46 +0200

    Solve day05

--- src/day05.rs
+++ src/day05.rs
@@ -1,7 +1,88 @@
-pub fn solve_1() -> i64 {
-    -1
+fn build_input<'a>() -> (&'a str, Vec<Vec<char>>) {
+    let input = include_str!("../input/day05.txt");
+
+    let mut max_length = 0;
+    let mut num_stacks = 0;
+    for line in input.lines() {
+        if line.chars().nth(1).unwrap().is_ascii_digit() {
+            num_stacks = line.split("   ").count();
+            break;
+        }
+        max_length += 1;
+    }
+    assert!(
+        num_stacks < 10,
+        "input parsing for more than 9 stacks not supported"
+    );
+
+    let mut stacks = vec![Vec::new(); num_stacks];
+    for line in input
+        .lines()
+        .take(max_length)
+        .collect::<Vec<_>>()
+        .iter()
+        .rev()
+    {
+        let mut idx = 1;
+        for stack in &mut stacks {
+            let item = line.chars().nth(idx).unwrap();
+            if item != ' ' {
+                stack.push(item);
+            }
+            idx += 4;
+        }
+    }
+
+    (input, stacks)
 }
 
-pub fn solve_2() -> i64 {
-    -1
+pub fn solve_1() -> String {
+    let (input, mut stacks) = build_input();
+
+    for line in input.lines().skip(stacks.len() + 1) {
+        let parts = line.split_once(" from ").unwrap();
+        let count = parts.0.split_once(' ').unwrap().1.parse::<usize>().unwrap();
+        let (src, dst) = parts.1.split_once(" to ").unwrap();
+        let (src, dst) = (
+            src.parse::<usize>().unwrap() - 1,
+            dst.parse::<usize>().unwrap() - 1,
+        );
+        for _ in 0..count {
+            let tmp = stacks[src].pop().unwrap();
+            stacks[dst].push(tmp);
+        }
+    }
+
+    let mut output = String::new();
+    for stack in stacks {
+        output.push(*stack.last().unwrap());
+    }
+    output
+}
+
+pub fn solve_2() -> String {
+    let (input, mut stacks) = build_input();
+
+    for line in input.lines().skip(stacks.len() + 1) {
+        let parts = line.split_once(" from ").unwrap();
+        let count = parts.0.split_once(' ').unwrap().1.parse::<usize>().unwrap();
+        let (src, dst) = parts.1.split_once(" to ").unwrap();
+        let (src, dst) = (
+            src.parse::<usize>().unwrap() - 1,
+            dst.parse::<usize>().unwrap() - 1,
+        );
+        let mut tmp = Vec::new();
+        for _ in 0..count {
+            tmp.push(stacks[src].pop().unwrap());
+        }
+        for item in tmp.iter().rev() {
+            stacks[dst].push(*item);
+        }
+    }
+
+    let mut output = String::new();
+    for stack in stacks {
+        output.push(*stack.last().unwrap());
+    }
+    output
 }
