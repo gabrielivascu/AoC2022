dc91fac22c7ffcf0fd080e7f47a80ba055c67efb
Gabriel Ivașcu <ivascu.gabriel59@gmail.com>
2022-12-06 08:36:52 +0200

    Solve day 06

--- src/day06.rs
+++ src/day06.rs
@@ -1,9 +1,20 @@
-pub fn solve_1(_input: &str) -> i64 {
-    -1
+use itertools::Itertools;
+
+pub fn solve_1(input: &str) -> usize {
+    solver(input, 4)
 }
 
-pub fn solve_2(_input: &str) -> i64 {
-    -1
+pub fn solve_2(input: &str) -> usize {
+    solver(input, 14)
+}
+
+fn solver(input: &str, length: usize) -> usize {
+    for i in length - 1..input.len() {
+        if input[i + 1 - length..=i].chars().unique().count() == length {
+            return i + 1;
+        }
+    }
+    0
 }
 
 #[cfg(test)]
@@ -12,11 +23,11 @@ mod tests {
 
     #[test]
     fn test_1() {
-        assert_eq!(solve_1(include_str!("../input/day06-sample.txt")), -1);
+        assert_eq!(solve_1(include_str!("../input/day06-sample.txt")), 7);
     }
 
     #[test]
     fn test_2() {
-        assert_eq!(solve_2(include_str!("../input/day06-sample.txt")), -1);
+        assert_eq!(solve_2(include_str!("../input/day06-sample.txt")), 19);
     }
 }
