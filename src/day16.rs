bbb74b1f1a92320eba46da2490305e4eff6fdac8
Gabriel Ivașcu <ivascu.gabriel59@gmail.com>
2022-12-17 16:53:05 +0200

    Day 16: Tweaks

--- src/day16.rs
+++ src/day16.rs
@@ -9,31 +9,31 @@ pub fn solve_1(input: &str) -> usize {
 
     for minute in 1..30 {
         let mut new_states = HashMap::new();
-        for (p, s) in &states {
+        for (progress, state) in &states {
             // Build the next states by moving to each neighboring valve.
-            for neighbor in &graph.get(p.valve).unwrap().neighbors {
+            for neighbor in &graph.get(progress.valve).unwrap().neighbors {
                 // Move to the new valve and consume a minute. No new valve is
-                // opened. No progress to the total release is made.
+                // opened. No progress to the total pressure release is made.
                 new_states.insert(
-                    Progress::new(neighbor, p.release),
-                    State::new(s.opened.clone(), s.minute + 1),
+                    Progress::new(neighbor, progress.pressure),
+                    State::new(state.opened.clone(), state.minute + 1),
                 );
             }
             // Open the current valve if not already opened and it's worth opening.
-            let flow = graph.get(p.valve).unwrap().flow;
-            if !s.opened.contains(p.valve) && flow > 0 {
-                let mut new_opened = s.opened.clone();
-                new_opened.insert(p.valve);
+            let flow = graph.get(progress.valve).unwrap().flow;
+            if !state.opened.contains(progress.valve) && flow > 0 {
+                let mut new_opened = state.opened.clone();
+                new_opened.insert(progress.valve);
                 new_states.insert(
-                    Progress::new(p.valve, p.release + flow * (30 - minute)),
-                    State::new(new_opened, s.minute + 1),
+                    Progress::new(progress.valve, progress.pressure + flow * (30 - minute)),
+                    State::new(new_opened, state.minute + 1),
                 );
             }
         }
         states = new_states;
     }
 
-    states.keys().map(|p| p.release).max().unwrap()
+    states.keys().map(|p| p.pressure).max().unwrap()
 }
 
 pub fn solve_2(input: &str) -> usize {
@@ -47,60 +47,60 @@ pub fn solve_2(input: &str) -> usize {
 
     for minute in 1..=26 {
         let mut new_states = HashMap::new();
-        for (p, s) in &states {
+        for (progress, state) in &states {
             // Compound each possible state: both stay in their previous positions,
             // I stay and the elephant moves, I move and the elephant stays.
             for (my_valve, elephant_valve) in iproduct!(
                 graph
-                    .get(p.me)
+                    .get(progress.me)
                     .unwrap()
                     .neighbors
                     .iter()
-                    .chain([p.me].iter()),
+                    .chain([progress.me].iter()),
                 graph
-                    .get(p.elephant)
+                    .get(progress.elephant)
                     .unwrap()
                     .neighbors
                     .iter()
-                    .chain([p.elephant].iter())
+                    .chain([progress.elephant].iter())
             ) {
-                let mut new_state = State::new(s.opened.clone(), s.minute + 1);
-                let mut new_release = p.release;
+                let mut new_state = State::new(state.opened.clone(), state.minute + 1);
+                let mut new_pressure = progress.pressure;
 
                 let my_flow = graph.get(my_valve).unwrap().flow;
                 let elephant_flow = graph.get(elephant_valve).unwrap().flow;
 
-                if *my_valve == p.me && *elephant_valve == p.elephant {
+                if *my_valve == progress.me && *elephant_valve == progress.elephant {
                     // Both spend the minute to open their respective valves.
                     if !new_state.opened.contains(my_valve) && my_flow > 0 {
                         new_state.opened.insert(my_valve);
-                        new_release += my_flow * (26 - minute);
+                        new_pressure += my_flow * (26 - minute);
                     }
                     if !new_state.opened.contains(elephant_valve) && elephant_flow > 0 {
                         new_state.opened.insert(elephant_valve);
-                        new_release += elephant_flow * (26 - minute);
+                        new_pressure += elephant_flow * (26 - minute);
                     }
-                } else if *my_valve == p.me {
+                } else if *my_valve == progress.me {
                     // I spend the minute opening my valve while the elephant moves.
                     if !new_state.opened.contains(my_valve) && my_flow > 0 {
                         new_state.opened.insert(my_valve);
-                        new_release += my_flow * (26 - minute);
+                        new_pressure += my_flow * (26 - minute);
                     }
-                } else if *elephant_valve == p.elephant {
+                } else if *elephant_valve == progress.elephant {
                     // The elephant spends the minute opening its valve while I move.
                     if !new_state.opened.contains(elephant_valve) && elephant_flow > 0 {
                         new_state.opened.insert(elephant_valve);
-                        new_release += elephant_flow * (26 - minute);
+                        new_pressure += elephant_flow * (26 - minute);
                     }
                 }
 
-                let new_progress = ProgressTwo::new(my_valve, elephant_valve, new_release);
-                // Make sure not to insert needless equivalent states, i.e. switched positions with same total release.
+                let new_progress = ProgressTwo::new(my_valve, elephant_valve, new_pressure);
+                // Make sure not to insert needless equivalent states, i.e. switched positions with same total pressure.
                 if !new_states.contains_key(&new_progress)
                     && !new_states.contains_key(&ProgressTwo::new(
                         elephant_valve,
                         my_valve,
-                        new_release,
+                        new_pressure,
                     ))
                 {
                     new_states.insert(new_progress, new_state);
@@ -110,7 +110,7 @@ pub fn solve_2(input: &str) -> usize {
         states = new_states;
     }
 
-    states.keys().map(|p| p.release).max().unwrap()
+    states.keys().map(|p| p.pressure).max().unwrap()
 }
 
 #[derive(Debug)]
@@ -127,13 +127,13 @@ impl Valve<'_> {
 
 #[derive(Debug, PartialEq, Eq, Hash)]
 struct Progress<'a> {
-    valve: &'a str, // valve where I am
-    release: usize, // total release available
+    valve: &'a str,  // valve where I am
+    pressure: usize, // total pressure released
 }
 
 impl Progress<'_> {
-    fn new(valve: &str, release: usize) -> Progress {
-        Progress { valve, release }
+    fn new(valve: &str, pressure: usize) -> Progress {
+        Progress { valve, pressure }
     }
 }
 
@@ -141,15 +141,15 @@ impl Progress<'_> {
 struct ProgressTwo<'a> {
     me: &'a str,       // valve where I am
     elephant: &'a str, // valve where the elephant is
-    release: usize,    // total release available
+    pressure: usize,   // total pressure released
 }
 
 impl ProgressTwo<'_> {
-    fn new<'a>(me: &'a str, elephant: &'a str, release: usize) -> ProgressTwo<'a> {
+    fn new<'a>(me: &'a str, elephant: &'a str, pressure: usize) -> ProgressTwo<'a> {
         ProgressTwo {
             me,
             elephant,
-            release,
+            pressure,
         }
     }
 }
